949d4198865d63790f234d71ac94f618145417bb
Chooook <egr311@ya.ru>
2024-08-11 20:50:55 +0000

    test: upgrade tests

--- test.py
+++ test.py
@@ -1,44 +1,17 @@
+from random import shuffle
 from OOEContest import OOEContest
 
 
 if __name__ == '__main__':
     instance = OOEContest()
-    print(instance.get_judge_data('login'))
+    login = 'login7'
+    print(instance.get_judge_data(login))
+    participants = list(range(1, 31))
+    shuffle(participants)
 
-    print(instance.update_votes('login', 'checkbox1_2'))
-    print(instance.update_votes('login', 'checkbox3_2'))
-    print(instance.update_votes('login', 'checkbox3_2'))
-    print(instance.update_votes('login', 'checkbox20_2'))
-    print(instance.update_votes('login', 'checkbox4_2'))
-    print(instance.update_votes('login', 'checkbox5_2'))
-    print(instance.update_votes('login', 'checkbox6_2'))
-    print(instance.update_votes('login', 'checkbox7_2'))
-    print(instance.update_votes('login', 'checkbox8_2'))
-    print(instance.update_votes('login', 'checkbox9_2'))
-    print(instance.update_votes('login', 'checkbox10_2'))
-    print(instance.update_votes('login', 'checkbox11_2'))
-    print(instance.update_votes('login', 'checkbox12_2'))
-    print(instance.update_votes('login', 'checkbox13_2'))
-    print(instance.update_votes('login', 'checkbox14_2'))
-    print(instance.update_votes('login', 'checkbox15_2'))
-    print(instance.update_votes('login', 'checkbox16_2'))
-    print(instance.update_votes('login', 'checkbox17_2'))
+    for i in range(15):
+        print(instance.update_votes(login, f'checkbox{participants.pop()}_1'))
 
-    print(instance.update_votes('login', 'checkbox10_1'))
-    print(instance.update_votes('login', 'checkbox11_1'))
-    print(instance.update_votes('login', 'checkbox12_1'))
-    print(instance.update_votes('login', 'checkbox13_1'))
-    print(instance.update_votes('login', 'checkbox14_1'))
-    print(instance.update_votes('login', 'checkbox15_1'))
-    print(instance.update_votes('login', 'checkbox16_1'))
-    print(instance.update_votes('login', 'checkbox17_1'))
-    print(instance.update_votes('login', 'checkbox20_1'))
-    print(instance.update_votes('login', 'checkbox21_1'))
-    print(instance.update_votes('login', 'checkbox22_1'))
-    print(instance.update_votes('login', 'checkbox23_1'))
-    print(instance.update_votes('login', 'checkbox24_1'))
-    print(instance.update_votes('login', 'checkbox25_1'))
-    print(instance.update_votes('login', 'checkbox26_1'))
-
-    print(instance.finish_voting('login'))
-    print(instance.renew_voting('login'))
+    print(instance.finish_voting(login))
+    print(instance.renew_voting(login))
+    instance.save_results_to_excel()
