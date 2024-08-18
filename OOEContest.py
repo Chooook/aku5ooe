7a2729d53316269e10cb4bff149329eb1f7d0146
Chooook <egr311@ya.ru>
2024-08-18 19:39:24 +0000

    major: update with remote changes

--- OOEContest.py
+++ OOEContest.py
@@ -1,7 +1,5 @@
 """Модуль для сохранения результатов соревнования"""
-
 import io
-import json
 import os
 from string import Template
 
@@ -11,17 +9,16 @@ import smbclient
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes, serialization
 from cryptography.hazmat.primitives.asymmetric import padding
-from http import HTTPStatus
 from smbclient.shutil import open_file
 
 
 class OOEContest:
-    def __init__(self, str_rsa_public=None):
+    def __init__(self, str_rsa_public=""):
         """Класс для работы с голосованием по конкурсу"""
         current_dir = os.path.dirname(os.path.realpath(__file__))
-        self.filename_template = Template('$login.enc')
         self.output_path = current_dir
         self.fir_output_path = r'alt'
+        self.filename_template = Template('$login.enc')
 
         self.__judges_dict = {
             'login1': 'judge1name',
@@ -65,29 +62,66 @@ class OOEContest:
             'participant29': 'partname29',
             'participant30': 'partname30',
         }
-        if not str_rsa_public:
-            str_rsa_public = """
-                -----BEGIN PUBLIC KEY-----
-                MIIBIDALBgkqhkiG9w0BAQoDggEPADCCAQoCggEBAMYqsw0eOig+dluYmF8986lc
-                Jqo/WSgL5RNeaI+ZXBeYsMON9iqvkJmaro3O1Y+VM3cwnAhMQxw59lFjv5BFVM6B
-                zWWisUMHia67HI89M7ktD/YbpB1zAQ9GNv65XRrpQyhYe/mbC43jXMZ/dpSzfqdb
-                gRXk741pAQZUJaAXN1ewgIL+JITV6VbtDJCUsn6OLKpidnBQs5OsfjDIk3jVaBBA
-                JeyAktuFq+8UJOSs9sep2mpY//HTGvbgBVZyCE13arnecgUZHolyfAUyh2Poqr/F
-                uEsrygp23JiE3EIXoESMcT1+e/+2BaMbGaSSt9qwdhLTWzn/JkkrP1/fztRbCfUC
-                AwEAAQ==
-                -----END PUBLIC KEY-----
-                """
+
+        self.__str_rsa_public = str_rsa_public
+        self.__default_str_rsa_public = """
+-----BEGIN PUBLIC KEY-----
+MIIBIDALBgkqhkiG9w0BAQoDggEPADCCAQoCggEBAMYqsw0eOig+dluYmF8986lc
+Jqo/WSgL5RNeaI+ZXBeYsMON9iqvkJmaro3O1Y+VM3cwnAhMQxw59lFjv5BFVM6B
+zWWisUMHia67HI89M7ktD/YbpB1zAQ9GNv65XRrpQyhYe/mbC43jXMZ/dpSzfqdb
+gRXk741pAQZUJaAXN1ewgIL+JITV6VbtDJCUsn6OLKpidnBQs5OsfjDIk3jVaBBA
+JeyAktuFq+8UJOSs9sep2mpY//HTGvbgBVZyCE13arnecgUZHolyfAUyh2Poqr/F
+uEsrygp23JiE3EIXoESMcT1+e/+2BaMbGaSSt9qwdhLTWzn/JkkrP1/fztRbCfUC
+AwEAAQ==
+-----END PUBLIC KEY-----
+"""
+        if not self.__str_rsa_public:
+            self.__str_rsa_public = self.__default_str_rsa_public
         self.__rsa_public = serialization.load_pem_public_key(
-            str_rsa_public.encode(),
+            self.__str_rsa_public.encode(),
             backend=default_backend()
         )
+        self.__default_str_rsa_private = """
+-----BEGIN PRIVATE KEY-----
+MIIEuwIBADALBgkqhkiG9w0BAQoEggSnMIIEowIBAAKCAQEAyb2TG9kMmsXpRyEw
+sTeaxng/rr/uZIzaDQXu9U6TuEQkLKEwRlkX5LxUqrAI9vxPAMi+YQu1fVDwJakw
+TMPIZlOLXjpSvJp7CIg1y7BSGM19YZP6cxhgXPQcvAjaJcDndBNXpG1SJ8sdDGv4
+NVfqs75BOxhaqMzLYO0DgzAOeCDh44ihcsCrz4tl5sBE5peLk4VCdGapWjdVZ5Pb
+h1h+sNgOavomHxqZbgo5yGIp2tFdWaDYdtYHNiCocihBXGSR5hBgAAJcyiPDTT0a
+fUo5/D/y8CPvk6z5vctqaRXAmkCzYlBNAn7fI8V21+t/i8V9qIO5Y4zkkidfOYS6
+Y2pFcwIDAQABAoIBADk8XTvF4TMaGlyRWJC232UWoa6xCnAhnA2c1NZpBDT/tCmr
+U5hp14MQQY8poWl9XOayjXzYBxY2O+Pbc/Ybh8QsSLqjnmyfAXACwQx4ilo6Fqv0
+AVbdIB3PLkXU1xtl9uSyRifC0k+y6xtmmIV923tCa5xaBQKmE798jwYNwNUosWby
+3XZ6/fHo7ngpQJJlzCUP4sTNuejMuQgRHVCTt8/ZncuzMZ3ruLkNi7WqHkdZ8VnN
+c9LXL6whkJAohz73i3+gipIyE8/+Zc05yNOJyNgDLMnnVa/TtJ8ls9wp/XTlt1H8
+HxnhWNJRWw0wxbf+Di1n7XEibbCXAawnplIFxDECgYEA/VktYOOgLT+zKfO80Nsf
+iTXdO8OAqiR2LpEisE96V+nykn8FJEmBaXVmPdGO/qPf2o36VLMCin56Dv9VW9Uj
+WjcdEwk8EPDBMjunux02U/QPsKStaqAnJXZpEVQdnD36cDuz5bLxL2AOrmVt5s78
+cHG9auv2lZ/eE59u/PBiVhECgYEAy9oegEDOjbxhORrXjM4Qbhq29ct1G6spVN4p
+/tcsQqLU212igOEcYhH06RB4emVxT9tCbLZ9zqJVkhDQMUR+0TCuY0dvEUHj2nct
+oMio673hiOr+dz/nVG3LCqqqrF1jJujMl1ynmHysnCy+Od7bpnLBLEzkwWNM4Nq6
+tGa2z0MCgYEA9+24adP4oa9v3wNG6UE3GGjdCypklJzABwxDXTU6LiSlHVYuqvdA
+LPsVxjN485tdax18OD1CpFPnkRuw5gCr6xJ6YnGsFYv2FPmqSIPq8berTxupFeqK
+xK+fXLTrkUZZ+lGC2KwIOWuQknxyU+iFxGiajLNEieJ8SsnArMl2AnECgYByG7WJ
+Gz3EDxfpDEJuOgbuaxvROMNj6oqnS/j8AtxurJEz/hTxyZDGwMB0GdkmwlQMXHKx
+QfHoUexOaATyHyJR3MsxHZJpeZWe+6lZ4BjWZSKzLr+kZuwJ0a+fV+tTsq7G3/du
+HtpdvCQvA8izwjD32jKRprVCH2CwWR+7zec02wKBgG02iVAexKguHjaQDe1IPwb1
+P5SaHr3UzmoeRUWOY7SQl5OiAkvt/FIknbcQ7FHmeN24G5IvDS4tH391L5QSdKcc
+Ma/5UHTIkjxUsTJWGwDU/blHLaAXLoOGMWmaQNXY0UiPvtdV1UJeILFURi5G59B1
+lx8fAoXVy9Dac4OTr4Hy
+-----END PRIVATE KEY-----
+"""
+
+    @staticmethod
+    def check_return(param):
+        return str(param)
 
     @staticmethod
     def showVersion():
         """Версия программы для AKU5"""
-        return 'OOEContest 2024-08-12 v0.4'
+        return 'OOEContest 2024-08-16 v0.10'
 
-    def save_data(self, login: str, data: str):
+    def save_data(self, login: str, data: list):
         """Метод для сохранения данных о голосовании по логину судьи"""
 
         # В переменной data должен быть логин и список чек-боксов с like
@@ -98,9 +132,9 @@ class OOEContest:
             checkbox = checkbox.split('_')[0]
             number = ''.join(list(filter(lambda x: x.isdigit(), checkbox)))
             return number
+
         try:
             fernet, fernet_key_encrypted = self.__get_fernet_key()
-            data = json.loads(data)
             participants = [
                 f'participant{get_participant(checkbox)}' for checkbox in data
             ]
@@ -115,9 +149,11 @@ class OOEContest:
             path = os.path.join(self.output_path, filename)
             with open(path, 'wb') as f:
                 f.write(encrypted_with_key)
-            return HTTPStatus(200)
-        except Exception:
-            return HTTPStatus(400)
+            if self.__default_str_rsa_public == self.__str_rsa_public:
+                return '300'
+            return '200'
+        except Exception as e:
+            return str(e)
 
     def __get_fernet_key(self):
         """Метод для получения симметричного ключа для шифрования"""
@@ -133,7 +169,7 @@ class OOEContest:
         )
         return fernet, fernet_key_encrypted
 
-    def __read_data(self, login: str, str_rsa_private: str):
+    def __read_data(self, login: str, str_rsa_private: bytes):
         """Метод для чтения данных о голосовании по логину"""
         filename = self.filename_template.substitute(login=login)
         path = os.path.join(self.output_path, filename)
@@ -141,9 +177,8 @@ class OOEContest:
             data = f.read()
         data, fernet_len = data.split(b'*****')
         data, fernet_key_encrypted = data[:-256], data[-256:]
-        str_rsa_private = str_rsa_private.replace(r'\n', '')
         private_rsa = serialization.load_pem_private_key(
-            str_rsa_private.encode(),
+            str_rsa_private,
             password=None,
             backend=default_backend()
         )
@@ -157,31 +192,39 @@ class OOEContest:
         )
         fernet = Fernet(fernet_key)
         decrypted_data = fernet.decrypt(data).decode()
-        os.remove(path)
+        # os.remove(path)
         return decrypted_data
 
     def save_results_to_excel(
-            self, str_rsa_private: str, judges: str = None):
+            self, str_rsa_private: str = "", judges: dict = None):
         """Метод для сохранения результатов голосования в excel файл"""
         # judges = '{"login1": "judge1name", "login2": "judge2name",...}'
+        return_code = '200'
+        if not str_rsa_private:
+            str_rsa_private = self.__default_str_rsa_private.encode()
+            return_code = '300'
+        else:
+            session = self.__create_smb_session()
+            with open_file(str_rsa_private, 'rb') as f:
+                str_rsa_private = f.read()
+            session.disconnect()
         try:
-            if judges:
-                judges_dict = json.loads(judges)
-            else:
-                judges_dict = self.__judges_dict
+            if not judges:
+                judges = self.__judges_dict
             columns = (
                     ['Ранг']
-                    + list(judges_dict.values())
+                    + list(judges.values())
                     + ['Сумма голосов']
             )
             index = list(self.__participants_dict.values()) + ['Сумма голосов']
             result_df = pd.DataFrame(index=index, columns=columns)
             for login in judges:
-                judge_name = judges_dict.get(login)
+                judge_name = judges.get(login)
                 try:
                     judge_data = self.__read_data(login, str_rsa_private)
                     judge_data = judge_data.split('\n')
-                except Exception:
+                except Exception as e:
+                    print(e)
                     continue
                 for participant in judge_data:
                     participant_name = self.__participants_dict.get(participant)
@@ -201,9 +244,10 @@ class OOEContest:
                 f.write(byte_stream.read())
             session.disconnect()
             byte_stream.close()
-            return HTTPStatus(200)
-        except Exception:
-            return HTTPStatus(400)
+            smbclient.reset_connection_cache()
+            return return_code
+        except Exception as e:
+            return str(e)
 
     @staticmethod
     def __create_smb_session():
@@ -211,7 +255,8 @@ class OOEContest:
         smb_server = 'server'
         smb_user = r'login'
         smb_pass = 'pass'
-        return smbclient.register_session(
+        session = smbclient.register_session(
             server=smb_server,
             username=smb_user,
             password=smb_pass)
+        return session
